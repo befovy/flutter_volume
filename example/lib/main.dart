080d45cf0ba5596418450836e4f31551fe1b4e8f
befovy <befovy@gmail.com>
2019-10-17 22:22:31 +0800

    feat: volume example pages

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,4 +1,3 @@
-import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_volume/flutter_volume.dart';
@@ -11,21 +10,18 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  bool sliding = false;
+  double sliderVal = 0;
+  double volume = 0;
 
   @override
   void initState() {
     super.initState();
-    initPlatformState();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
+    FlutterVolume.get().then((v) {
+      setState(() {
+        volume = v;
+      });
+    });
   }
 
   @override
@@ -35,42 +31,72 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: const Text('Plugin example app'),
         ),
-        body: Center(
+        body: VolumeWatcher(
+          watcher: (vol) {
+            setState(() {
+              volume = vol.vol;
+            });
+          },
+          child: Center(
             child: Column(
-          children: <Widget>[
-            FlatButton(
-                onPressed: () {
-                  FlutterVolume.up();
-                },
-                child: Text("up")),
-            FlatButton(
-                onPressed: () {
-                  FlutterVolume.down();
-                },
-                child: Text("down")),
-            FlatButton(
-                onPressed: () {
-                  FlutterVolume.mute();
-                },
-                child: Text("mute")),
-            Slider(
-              value: FlutterVolume.value.vol,
-              onChanged: (v) {
-                FlutterVolume.set(v);
-              },
+              children: <Widget>[
+                FlatButton(
+                    onPressed: () {
+                      FlutterVolume.up();
+                    },
+                    child: Text("up")),
+                FlatButton(
+                    onPressed: () {
+                      FlutterVolume.down();
+                    },
+                    child: Text("down")),
+                FlatButton(
+                    onPressed: () {
+                      FlutterVolume.mute();
+                    },
+                    child: Text("mute")),
+                Slider(
+                  value: sliding ? sliderVal : volume,
+                  onChangeStart: (v) {
+                    setState(() {
+                      sliding = true;
+                    });
+                  },
+                  onChangeEnd: (v) {
+                    FlutterVolume.set(v).then((_) {
+                      setState(() {
+                        sliding = false;
+                      });
+                    });
+                  },
+                  onChanged: (v) {
+                    FlutterVolume.set(v);
+                    setState(() {
+                      sliderVal = v;
+                    });
+                  },
+                ),
+
+                Expanded(child: Container(),),
+                FlatButton(
+                    onPressed: () {
+                      FlutterVolume.enableWatcher();
+                      FlutterVolume.get().then((v) {
+                        setState(() {
+                          volume = v;
+                        });
+                      });
+                    },
+                    child: Text("enable watch")),
+                FlatButton(
+                    onPressed: () {
+                      FlutterVolume.disableWatcher();
+                    },
+                    child: Text("disable watch")),
+              ],
             ),
-            FlatButton(
-                onPressed: () {
-                  FlutterVolume.enableWatcher();
-                },
-                child: Text("enable watch")),
-            FlatButton(
-                onPressed: () {
-                  FlutterVolume.disableWatcher();
-                },
-                child: Text("disable watch")),
-          ],
-        )),
+          ),
+        ),
       ),
     );
   }
